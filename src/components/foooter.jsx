2ab69065da062ed547bb7f4d4e92c16be78be854
AshishDochania <114864587+AshishDochania@users.noreply.github.com>
2024-02-06 17:31:47 +0530

    Add files via upload

--- src/components/foooter.jsx
+++ src/components/foooter.jsx
@@ -1,7 +1,7 @@
 export default function Foot(){
     return <footer className="foot1">
     <h6>Registration Fee : Rs 300</h6>
-      <a href="https://www.w3schools.com/tags/tag_button.asp">
+      <a href="https://docs.google.com/forms/d/e/1FAIpQLSdiO-8Fi7kYWQIgYkJ-XpQvlONlvHz6nBINUgpsKlwWYz2J3w/viewform?usp=pp_url" target="blank">
         <button placeholder="Register">
           Register
         </button>
